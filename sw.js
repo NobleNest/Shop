8c3ce66fd7ed14d01d54559ff72931cdc27cde68
DelphiHater <nesterov.gena1@gmail.com>
2020-04-02 16:21:33 +0300

    cache api in service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open("static")
+          .then(function(cache) {
+            console.log("precaching");
+            cache.add('/index.html');
+            cache.add('/css/bootstrap.css');
+            cache.add('/images/logo.png');
+            cache.add('/');
+          })
+    );
   });
-  self.addEventListener('activate', function(event) {
- console.log('[Service Worker] Activating Service Worker ...', event);
- return self.clients.claim();
- });
-   self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ....', event);
-    event.respondWith(fetch(event.request));
-  }); 
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ...', event);
+  return self.clients.claim();
+});
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if(response)
+            return response;
+          else
+            return fetch(event.request);
+        }
+      )
+    );
+  });
